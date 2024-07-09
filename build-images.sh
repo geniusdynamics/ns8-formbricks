57b0b790c68140fc6f19fc359ece0ac00464d8e3
Martin Bhuong <martin@genius.ke>
2024-07-09 21:37:18 +0200

    Bump to version 2.3.1

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="formbricks"
-APP_VERSION="2.3.0"
+APP_VERSION="2.3.1"
 
 # Create a new empty container image
 container=$(buildah from scratch)
