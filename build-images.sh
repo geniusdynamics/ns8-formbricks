7de51ec14ef684e50e987d68676d6fc8d9c8e2f7
Martin Otieno <martin@genius.ke>
2025-03-01 23:51:25 +0300

    pg15

--- build-images.sh
+++ build-images.sh
@@ -49,7 +49,7 @@ buildah config --entrypoint=/ \
 	--label="org.nethserver.authorizations=traefik@node:routeadm" \
 	--label="org.nethserver.tcp-ports-demand=1" \
 	--label="org.nethserver.rootfull=0" \
-	--label="org.nethserver.images=docker.io/pgvector/pgvector:pg17 docker.io/formbricks/formbricks:${APP_VERSION} docker.io/library/redis:7" \
+	--label="org.nethserver.images=docker.io/pgvector/pgvector:pg15 docker.io/formbricks/formbricks:${APP_VERSION} docker.io/library/redis:7" \
 	"${container}"
 
 # Commit the image
