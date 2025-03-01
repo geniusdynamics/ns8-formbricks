d28340c28b9319fac54d316fc4a89b541dcce0e4
Martin Otieno <martin@genius.ke>
2025-03-01 22:39:05 +0300

    update formbricks to 3.3.0

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="formbricks"
-APP_VERSION="3.1.4"
+APP_VERSION="3.3.0"
 
 # Create a new empty container image
 container=$(buildah from scratch)
